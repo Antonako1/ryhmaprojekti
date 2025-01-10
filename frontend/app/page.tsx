d639e07d0ce50c67f2fba34d5d018827774406c0
Empnor <eemeli.noronen@gmail.com>
2025-01-10 08:24:44 +0200

    header ready to go

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,35 +1,43 @@
 'use client'
 
+import Header from '@/components/header/header'; 
 import { useAuth } from '@/Utils/context/contextUser';
 import { UserRoles } from '@/Utils/Interfaces';
 import Link from 'next/link';
 import { useEffect } from 'react';
+
 export default function Home() {
   const { user, logout, authenticated } = useAuth();
+
   return (
-    <ul>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      {user?.role === UserRoles.Admin && (
+    <div>
+      {/* Include the Header component */}
+      <Header />
+      
+      <ul>
         <li>
-          <Link href="/dashboard">Dashboard</Link>
+          <Link href="/">Home</Link>
         </li>
-      )} 
-      <li>
-        <Link href="/login-register">Login</Link>
-      </li>
-      {authenticated && (
+        {user?.role === UserRoles.Admin && (
+          <li>
+            <Link href="/dashboard">Dashboard</Link>
+          </li>
+        )}
+        <li>
+          <Link href="/login-register">Login</Link>
+        </li>
+        {authenticated && (
           <li>
             <button onClick={logout}>Logout</button>
           </li>
         )}
-      <li>
-        <Link href="/reviews">Reviews</Link>
-      </li>
-      <li>
-        <Link href="/blog/hello-world">Blog Post</Link>
-      </li>
-    </ul>
+        <li>
+          <Link href="/reviews">Reviews</Link>
+        </li>
+        <li>
+          <Link href="/blog/hello-world">Blog Post</Link>
+        </li>
+      </ul>
+    </div>
   );
 }
