8309f957ae82a4f2a04cd82d00c01212624be48b
antonako1-tutors <anton.akonpelto@gmail.com>
2025-01-15 16:17:34 +0200

    Stashi takas

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 
 import AddProduct from "@/components/Add/AddMain";
 import RollingCatalog from "@/components/RollingCatalog/RollingCatalog";
-import { server } from "@/Utils/consts";
+import { server, Types } from "@/Utils/consts";
 import { useAuth } from "@/Utils/context/contextUser";
 import { UserRoles } from "@/Utils/Interfaces";
 import { useRouter } from "next/navigation";
@@ -97,11 +97,11 @@ const Dashboard = () => {
     }
     return (
         <div>
-            <RollingCatalog props={{TO_FETCH: "CARS"}}/>
-            <RollingCatalog props={{TO_FETCH: "ALCOHOL"}}/>
-            <AddProduct props={{TYPE: "CARS"}}/>
+            <RollingCatalog props={{TO_FETCH: Types.CARS}}/>
+            <RollingCatalog props={{TO_FETCH: Types.ALCOHOL}}/>
+            <AddProduct props={{TYPE: Types.CARS}}/>
             <hr />
-            <AddProduct props={{TYPE: "ALCOHOL"}}/>
+            <AddProduct props={{TYPE: Types.ALCOHOL}}/>
 
             <button onClick={debug1}>Debug cars</button>
             <button onClick={debug2}>Debug alcohol</button>
--- frontend/components/ItemPage/ItemPage.tsx
+++ frontend/components/ItemPage/ItemPage.tsx
@@ -3,12 +3,17 @@
 import { IAlcoholDetails, ICarDetails } from "@/Utils/Interfaces";
 import styles from "./ItemPage.module.css";
 import { useState } from "react";
+import { Types } from "@/Utils/consts";
 
 interface ItemPageProps {
   props: {
     item: ICarDetails | IAlcoholDetails | null;
     error: string | null;
+<<<<<<< Updated upstream
     type: Types
+=======
+    type: Types;
+>>>>>>> Stashed changes
   };
 }
 
@@ -25,6 +30,7 @@ const ItemPage = ({ props }: ItemPageProps) => {
 
     if (props.error) return <div className={styles.error}>Error: {props.error}</div>;
     if (!props.item) return <div className={styles.error}>Item not found</div>;
+<<<<<<< Updated upstream
 
     const handleCartAdd = async () => {
 
@@ -64,6 +70,25 @@ const ItemPage = ({ props }: ItemPageProps) => {
                         </div>
                     </div>
                 </div>
+=======
+    return (
+        <div className={styles.itemPage}>
+        <h1 className={styles.pageTitle}>Looking for some <i>{props.item.productDetails.name}</i>?</h1>
+        <div className={styles.innerWrap}>
+            <img
+            src={props.item.productDetails.image}
+            alt={props.item.productDetails.name}
+            className={styles.productImage}
+            />
+            <div className={styles.itemInfo}>
+                <h2 className={styles.productTitle}>{props.item.productDetails.name}</h2>
+                <p className={styles.productDescription}>
+                    {props.item.productDetails.description}
+                </p>
+                <p className={styles.productPrice}>Price: {props.item.productDetails.price}€</p>
+                <p className={styles.productId}>Rating: {averageRating([1,2,3])}/5</p>
+
+>>>>>>> Stashed changes
             </div>
         </div>
     );
